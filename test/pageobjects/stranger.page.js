fb8ed4b93e4e59f3e49b19280f0ce1a5aa963484
elianbertona <elian.bertona@darwoft.com>
2026-04-03 11:42:18 -0300

    revert introduced failed

--- test/pageobjects/stranger.page.js
+++ test/pageobjects/stranger.page.js
@@ -5,7 +5,7 @@ class StrangerPage {
     return $("#inputImage");
   }
   get inputText() {
-    return $('textarea[name="text1234512345"]');
+    return $('textarea[name="text"]');
   }
   get btnCreate() {
     return $("button=Create Item");
